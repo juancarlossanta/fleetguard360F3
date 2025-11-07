5d539443283521faf78ab68020c12b24f6cf9f09
Juan Santa <jcsantahurtado@gmail.com>
2025-11-07 17:51:41 -0500

    Solving some issues

--- src/main/java/com/udea/fleetguard360F3/config/SecurityConfig.java
+++ src/main/java/com/udea/fleetguard360F3/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.udea.fleetguard360F3.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -18,12 +17,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/graphql").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(AbstractHttpConfigurer::disable);
+                .httpBasic(httpBasic -> httpBasic.disable());
         return http.build();
     }
 
